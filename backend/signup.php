cc5d73316a6dfbeb54182d00f85c7409f8192741
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-14 16:58:31 +0300

    Sign up improved, with error messages, verfications, POST tested

--- backend/signup.php
+++ backend/signup.php
@@ -2,6 +2,7 @@
 
 include('connect.php');
 
+//Check if body is all sent, if not return an error
 if(isset($_POST['user']) && isset($_POST['pass']) && isset($_POST['email'])) {
     $user = $_POST['user'];
     $pass = $_POST['pass'];
@@ -14,6 +15,7 @@ if(isset($_POST['user']) && isset($_POST['pass']) && isset($_POST['email'])) {
     exit();
 }
 
+//Check if user has set a number, if not continue normally with NULL as value
 if(isset($_POST['pnumber'])){
     $pnumber = $_POST['pnumber'];
 }
@@ -21,8 +23,26 @@ else{
     $pnumber = NULL;
 }
 
+//Checking if username or email exists in the backend
+$query = $mysqli->prepare("SELECT user FROM users WHERE user='$user' OR email='$email'");
+$query->execute();
+$results = $query->get_result();
 
-$query = $mysqli->prepare("INSERT INTO `users` (`id`, `user`, `password`, `email`, `phone_number`, `fname`, `lname`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL);");
+while($a = $results->fetch_assoc()){
+    $reply[] = $a;
+}
+
+//If user or email exists, exit the script and return an error
+if($reply){
+    $response = [];
+    $response["success"] = false;
+    $response["message"] = "User or email already exists!";
+    echo json_encode($response);
+    exit();
+}
+
+//Inserting new user
+$query = $mysqli->prepare("INSERT INTO `users` (`id`, `user`, `pass`, `email`, `phone_number`, `fname`, `lname`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL);");
 $query->bind_param("ssss", $user, $pass, $email, $pnumber);
 $query->execute();
 
